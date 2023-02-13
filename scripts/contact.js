a2a8fd55b1a46c75b42b310a49382b0b6eb40296
Liam Barton <liam.barton@dcmail.ca>
2023-02-12 23:10:40 -0500

    final phase

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,21 +1,31 @@
-document.getElementById("Desc1a").innerHTML = "The developers of this website are top classs programmers who attend Durham college enrolled in the Computer Programming Course and ready to make millions of dollars";
-//add another resume here
-document.getElementById("aboutresume1").innerHTML = "Liam's Resume -- password=123: <a href=\"https://www.protectedtext.com/-Lresume\">Resume</a> ";
+
+/* function to return the tabel and user information */
+
+function validateForm() {
+    let name = document.forms["myForm"]["username"].value;
+    let email = document.forms["myForm"]["emailAddress"].value;
+    let num = document.forms["myForm"]["phoneNumber"].value;
+    let message = document.forms["myForm"]["message"].value;
+
+    
+    
+        console.log(`Username: ${ name } \n Password: ${ email } \n PhoneNumebr: ${ num } \n Optional Message: ${ message }`);
+    
+        return false;
+    
+}
 
 
-
+    
 // get id for a tag in list 
 var navbarProject = document.getElementById("projectsNav");
 
-
 //make a variable with the value projects
+
 newtitle = "Projects";
 //change the a tag value to "projects"
 navbarProject.textContent = newtitle;
-
 navbarProject.setAttribute("class", "nav-link");
-
-
 
 //get the id HR from the list and then create a a tag, after that set the attribute of the a tag to lav link. Then
 //add the text "Human Resources to the a tag then add the a tag to the li"
